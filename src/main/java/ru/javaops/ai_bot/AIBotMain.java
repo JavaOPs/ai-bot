372edd46b8adbe102232d4315dc874671e693b39
JavaOPs <admin@javaops.ru>
2025-12-26 01:19:38 +0300

    2_3_spring_boot

--- src/main/java/ru/javaops/ai_bot/AIBotMain.java
+++ src/main/java/ru/javaops/ai_bot/AIBotMain.java
@@ -1,15 +1,27 @@
 package ru.javaops.ai_bot;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
+@SpringBootApplication
 @Slf4j
-public class AIBotMain {
-    public static void main(String[] args) throws Exception {
-        String botToken = "BOT_HTTP_API_Token";
+@RequiredArgsConstructor
+public class AIBotMain implements CommandLineRunner {
+    private final AIBot aiBot;
+
+    static void main(String[] args) {
+        SpringApplication.run(AIBotMain.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
         // https://rubenlagus.github.io/TelegramBotsDocumentation/getting-started.html
         try (TelegramBotsLongPollingApplication botsApp = new TelegramBotsLongPollingApplication()) {
-            botsApp.registerBot(botToken, new AIBot(botToken));
+            botsApp.registerBot(aiBot.getToken(), aiBot);
             log.info("Bot registered");
             Thread.currentThread().join();  // wait
         }
