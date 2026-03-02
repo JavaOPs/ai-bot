6460681aea8e07e286a02b6c76128e9a5312b6e2
JavaOPs <admin@javaops.ru>
2026-03-02 19:07:36 +0300

    1_1_sendMessage

--- src/main/java/ru/javaops/ai_bot/AIBotMain.java
+++ src/main/java/ru/javaops/ai_bot/AIBotMain.java
@@ -3,13 +3,15 @@ package ru.javaops.ai_bot;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
+import java.util.Objects;
+
 @Slf4j
 public class AIBotMain {
     public static void main(String[] args) throws Exception {
-        String botToken = "BOT_HTTP_API_Token";
+        String botToken = Objects.requireNonNull(System.getenv("BOT_HTTP_API_TOKEN"), "Set BOT_HTTP_API_TOKEN environment variable"); //"BOT_HTTP_API_Token";
         // https://rubenlagus.github.io/TelegramBotsDocumentation/getting-started.html
         try (TelegramBotsLongPollingApplication botsApp = new TelegramBotsLongPollingApplication()) {
-            botsApp.registerBot(botToken, new AIBot());
+            botsApp.registerBot(botToken, new AIBot(botToken));
             log.info("Bot registered");
             Thread.currentThread().join();  // wait
         }
